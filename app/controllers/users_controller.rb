2cd0b8dee2bbb524280ce16b8203ab96709fd20a
Vasim Shaikh <avasimshaikh@gmail.com>
2021-12-25 17:16:24 +0530

    completed sign up functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,21 @@ class UsersController < ApplicationController
         @user = User.new    
     end    
 
+    def create
+        @user = User.new(get_params)
+        if @user.save
+           flash[:notice] = "Welcome to Reactor6"
+           redirect_to articles_path
+        else 
+            render 'new'
+        end    
+    end
+    
+    
+    private
+
+    def get_params
+      params.require(:user).permit(:username, :email, :password_digest)
+    end
+
 end
---
+++ db/migrate/20211225112429_rename_password_name.rb
@@ -0,0 +1,5 @@
+class RenamePasswordName < ActiveRecord::Migration[6.1]
+  def change
+    rename_column :users, :paswword_digest, :pasword_digest
+  end
+end
